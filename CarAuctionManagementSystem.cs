bd813e2515b271d46adbd3d0842ee1aaa877187d
Renato Macedo <113690398+RenatoMcd@users.noreply.github.com>
2024-12-14 16:11:03 +0000

    Create logic for the Auction feature

--- CarAuctionManagementSystem.cs
+++ CarAuctionManagementSystem.cs
@@ -65,27 +65,118 @@ public class Truck : Vehicle
     }
 }
 
+public class Auction
+{
+    public Vehicle Vehicle { get; set; }
+    public bool IsActive { get; set; }
+    public decimal HighestBid { get; set; }
+    public decimal HighestBidder { get; set; }
+
+    public Auction(Vehicle vehicle)
+    {
+        Vehicle = vehicle;
+        IsActive = false;
+        HighestBid = vehicle.StartingBid;
+    }
+
+    public void StartAuction()
+    {
+        if (IsActive)
+        {
+            throw new Exception("Auction is already active.");
+        }
+        IsActive = true;
+    }
+
+    public void CloseAuction()
+    {
+        if (!IsActive)
+        {
+            throw new Exception("Auction is not active.");
+        }
+        IsActive = false;
+    }
+
+    public void PlaceBid(decimal bid, string bidder)
+    {
+        if (!IsActive)
+        {
+            throw new Exception("Can't place the bid, the auction is not active.");
+        }
+        if (bid <= HighestBid)
+        {
+            throw new Exception("Bid must be greatter than the current bid.");
+        }
+
+        HighestBid = bid;
+        HighestBidder = bidder;
+    }
+}
+
 public class CarAuctionManagementSystem
 {
-    public List<Vehicle> _auctionInventory = new List<Vehicle>();
-   
+    public List<Vehicle> _vehicleInventory = new List<Vehicle>();
+    public Dictionary<int, Auction> _auctions = new Dictionary<int, Auction>();
+
     public void AddVehicle(Vehicle vehicle)
     {
-        if (_auctionInventory.Any(v => v.Id == vehicle.Id))
+        if (_vehicleInventory.Any(v => v.Id == vehicle.Id))
         {
             throw new Exception("There is already a vehicle with this ID.")
         }
-        _auctionInventory.Add(vehicle);
+        _vehicleInventory.Add(vehicle);
     }
 
-    public IEnumerable<Vehicle> SearchVehicles(string type, string manufaturer, string model, int year) 
+    public IEnumerable<Vehicle> SearchVehicles(string type, string manufaturer, string model, int year)
     {
-        return _auctionInventory.Where(v =>
+        return _vehicleInventory.Where(v =>
             v.GetType().Name == type &&
             v.Manufacturer == manufaturer &&
             v.Model == model &&
             v.Year == year);
     }
+
+    public void StartAuction(int vehicleId)
+    {
+        var vehicle = _vehicleInventory.FirstOrDefault(v => v.Id == vehicleId);
+        
+        if (vehicle == null)
+        {
+            throw new Exception("There is no vehicle with this ID in the inventory.");
+        }
+        
+        if (_auctions.ContainsKey(vehicleId))
+        {
+            throw new Exception("This vehicle is already in an active auction.");
+        }
+        else
+        {
+            _auctions[vehicleId] = new Auction(vehicle);
+        }
+
+        _auctions[vehicleId].StartAuction();    
+    }
+
+    public void CloseAuction(int vehicleID)
+    {
+        if (!_auctions.ContainsKey(vehicleID) || !_auctions[vehicleID].IsActive)
+        {
+            throw new Exception("There is no active auction for this vehicle ID");
+        }
+
+        _auctions[vehicleID].CloseAuction();
+    }
+
+    public void PlaceBid(int vehicleID, decimal bid, string bidder)
+    {
+        if (!_auctions.ContainsKey(vehicleID) || !_auctions[vehicleID].IsActive)
+        {
+            throw new Exception("There is no active auction for this vehicle ID");
+        }
+
+        _auctions[vehicleID].PlaceBid(bid, bidder);
+    }
+}
 
 
 public class Program
